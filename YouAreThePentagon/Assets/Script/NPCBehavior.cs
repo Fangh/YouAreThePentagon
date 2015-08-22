12cea147f98e6560e691a80d9f26c12702e188eb
Fangh <toto.klein@laposte.net>
2015-08-22 12:54:13 +0200

    navmesh and NPC

---
+++ YouAreThePentagon/Assets/Script/NPCBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCBehavior : MonoBehaviour 
+{
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<NavMeshAgent> ().SetDestination (target.position); 
+	
+	}
+}
