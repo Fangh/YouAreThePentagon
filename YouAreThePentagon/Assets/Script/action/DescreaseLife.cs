8c04594731f054542d1fc7cca8f865357f3a7d5c
PLATEL Kevin <platel.kevin@gmail.com>
2015-08-22 14:23:37 +0200

    Adding action

---
+++ YouAreThePentagon/Assets/Script/action/DescreaseLife.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DescreaseLife : Action {
+
+    public int amount = 5;
+    public Life life;
+    public bool desactiveAfter = true;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    override public void run(TriggerAction trigger)
+    {
+        life.hit(amount);
+        if (desactiveAfter && trigger != null)
+            trigger.active = false;
+    }
+}
---
+++ YouAreThePentagon/Assets/Script/action/DescreaseMoney.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DescreaseMoney : Action {
+
+    public int amount = 5;
+    public Money money;
+    public bool desactiveAfter = true;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    override public void run(TriggerAction trigger)
+    {
+        money.decreaseMoney(amount);
+        if (desactiveAfter && trigger != null)
+            trigger.active = false;
+    }
+}
