1d50ef1d37ef1003aededc5a55c3bc807acaebc3
Fangh <toto.klein@laposte.net>
2015-08-22 14:22:25 +0200

    dialog

---
+++ YouAreThePentagon/Assets/Script/DialogAction.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class DialogAction : MonoBehaviour 
+{
+	public GameObject dialogPanel;
+	public string characterName;
+	[TextArea(3,10)]
+	public string text;
+
+	bool isRunning = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (!dialogPanel)
+			dialogPanel = GameObject.FindGameObjectWithTag ("DialogPanel");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyUp (KeyCode.Space) && !isRunning)
+			Run ();
+		else if (Input.GetKeyUp (KeyCode.Space) && isRunning)
+			Stop ();
+	
+	}
+
+	void Run()
+	{
+		isRunning = true;
+		GameObject.FindWithTag ("Player").GetComponent<MovePlayer>().canMove = false;
+		dialogPanel.transform.GetChild (0).gameObject.SetActive (true);
+		dialogPanel.transform.GetChild (0).GetChild (0).GetComponent<Text> ().text = characterName;
+		dialogPanel.transform.GetChild (0).GetChild (1).GetComponent<Text> ().DOText (
+			text, 2f, true, ScrambleMode.None, null);
+	}
+
+	void Stop()
+	{
+		isRunning = false;
+		GameObject.FindWithTag ("Player").GetComponent<MovePlayer>().canMove = true;
+		dialogPanel.transform.GetChild (0).gameObject.SetActive (false);
+		dialogPanel.transform.GetChild (0).GetChild (0).GetComponent<Text> ().text = "";
+		dialogPanel.transform.GetChild (0).GetChild (1).GetComponent<Text> ().text = "";
+	}
+}
--- YouAreThePentagon/Assets/Script/MovePlayer.cs
+++ YouAreThePentagon/Assets/Script/MovePlayer.cs
@@ -9,6 +9,8 @@ public class MovePlayer : MonoBehaviour {
     // State of the player. If the player colide with something, set it to true
     private bool collide = false;
 
+	public bool canMove = true;
+
 	// Use this for initialization
 	void Start () {
 	        
@@ -18,22 +20,19 @@ public class MovePlayer : MonoBehaviour {
     // return the newPosition
     public void move(Vector3 direction, Vector3 rotation)
     {
+		if (!canMove)
+			return;
+
         transform.Rotate(rotation * rotSpeed * Time.deltaTime);
         transform.Translate(direction * speed * Time.deltaTime, Space.Self);
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update () 
+	{
         float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
 
         move(new Vector3(0, 0, v), new Vector3(0, h, 0));
-//		transform.Rotate(new Vector3(0, h, 0) * rotSpeed * Time.deltaTime);
-//		transform.Translate(0, 0, v * speed * Time.deltaTime, Space.Self );
-
-//
-//        Vector3 direction = new Vector3(x, y, z);
-
-//        move(direction);
 	}
 }
