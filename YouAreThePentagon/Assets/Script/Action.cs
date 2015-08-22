136fd77cd7ac516fc5700a4dbbc678f00aaadf51
PLATEL Kevin <platel.kevin@gmail.com>
2015-08-22 16:44:16 +0200

    Adding random waiting time for NPC

--- YouAreThePentagon/Assets/Script/Action.cs
+++ YouAreThePentagon/Assets/Script/Action.cs
@@ -5,6 +5,7 @@ public abstract class Action : MonoBehaviour {
 
     public bool desactiveAfter = true;
     public bool active = true;
+    public float probability = 1.0f;
     // Use this for initialization
     void Start () {
 	
@@ -22,7 +23,7 @@ public abstract class Action : MonoBehaviour {
 
     public void Launch(TriggerAction trigger = null)
     {
-        if (active)
+        if (active && Random.Range(0.0f, 1.0f) <= probability)
             Run(trigger);
         if (desactiveAfter)
             active = false;
